a8a7392706199b362a97ef23c8ac5bbf42f1b537
Lara Martin <larikimc@gmail.com>
2016-03-03 15:08:46 +0100

    included button switch F-C

--- weather.js
+++ weather.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-	// $("#clickHere").on("click", function() {
-	// 	console.log("CLICKEDDDDDDDDDDDDD");
+	weather();
+	$("#myonoffswitch").on("click", function(){
+		weather();
+		console.log("olakaseee");
+	});
+});
 
+
+function weather(){
 		// geolocation
 		// https://developer.mozilla.org/es/docs/Web/API/Geolocation/getCurrentPosition
 		var options = {
@@ -16,24 +22,13 @@ $(document).ready(function() {
 
 		function success(pos) {
 			var crd = pos.coords;
-
-			console.log("========BEGIN TEST=====");
-
 			var lat = crd.latitude;
 			var lon = crd.longitude;
 			var key = "adb5c1c65e2b6af4260846cb9ec37296";
 			url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=metric";
 
-			// console.log("si que va....");
-			// console.log("lat: "  + lat);
-			// console.log("lat round: "  + Math.round(lat));
-			// console.log("lon: " + lon);
-			// console.log("url is: " + url);
-
 			// calling the JSON
 			weatherJS(url);
-
-
 		};
 
 		function error(err) {
@@ -42,51 +37,48 @@ $(document).ready(function() {
 
 		navigator.geolocation.getCurrentPosition(success, error, options);
 
-
 		function weatherJS(url){
 			$.getJSON(url, function(data){
 
 				var location = data.name;
 				var tempMax = Math.round(data.main.temp_max);
 				var tempMin = Math.round(data.main.temp_min);
+				// °C  x  9/5 + 32 = °F
+				var tempMaxF = tempMax * 9/5 + 32;
+				var tempMinF = tempMin * 9/5 + 32;
 				var forecast = data.weather[0].main;
 				var humidity = data.main.humidity;
 				var wind = data.wind.speed;
 				var icon = data.weather[0].icon;
-
 				var urlIcon = "http://openweathermap.org/img/w/" + icon + ".png";
-				console.log("icon url: " + urlIcon);
-
-				console.log("location: " + location);
-				console.log("weathermain: " + forecast);
-				console.log("temp min: " + tempMin);
-				console.log("temp max: " + tempMax);
-				console.log("icon: " + icon);
 
 				// local time
 				d = new Date();
 				var localTime = d.toLocaleString();
 
 				var html = "<div class= 'localWeather'>" +
-					"<div id='city'>" + location + "</div>" +
-					"<br><br>" + localTime +
-					"<br><br> Max: " + tempMax + "&#8451" +
-					"<br><br> Min: " + tempMin + "&#8451" +
-					"<br><br> Humidity: " + humidity + "%" +
-					"<br><br> Wind: " + wind + " meter/sec" +
-					"<br><br> Forecast: "+ forecast +
-					"<div class= 'imgWeather'><img src=\"" + urlIcon + "\"></div>"
-					 + "</div>";
+				"<div id='city'>" + location + "</div>" +
+				"<br><br>" + localTime +
+				"<br><br> Max: " + tempMax + "&#8451" +
+				"<br><br> Min: " + tempMin + "&#8451" +
+				"<br><br> Humidity: " + humidity + "%" +
+				"<br><br> Wind: " + wind + " meter/sec" +
+				"<br><br> Forecast: "+ forecast +
+				"<div class= 'imgWeather'><img src=\"" + urlIcon + "\"></div>"
+				+ "</div>";
 
 				$(".weatherInfo").html(html);
 
 			})
 		};
 
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-74642232-1', 'auto');
+		ga('send', 'pageview');
 
 
-
-	// });
-
-
-})
+};
